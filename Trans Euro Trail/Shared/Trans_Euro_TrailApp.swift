eb9d96554b6b716ce3d894133e29fd4e7a786e31
Robert <robert.zelhofer@gmail.com>
2022-02-25 09:14:58 +0100

    Add Xcode poject

---
+++ Trans Euro Trail/Shared/Trans_Euro_TrailApp.swift
@@ -0,0 +1,17 @@
+//
+//  Trans_Euro_TrailApp.swift
+//  Shared
+//
+//  Created by Rob on 25.02.22.
+//
+
+import SwiftUI
+
+@main
+struct Trans_Euro_TrailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
